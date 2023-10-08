e01cfdd9bce6345f820930e9efc7c914041cef83
myduh <myduh@isoftstone.com>
2023-10-08 09:51:38 +0800

    chore: adjust note position

--- src/spliceArray/index.ts
+++ src/spliceArray/index.ts
@@ -2,7 +2,8 @@
  * this func to delete target element for source array
  */
 const spliceArray = <ElementType>(source: ElementType[], targetIndexArray: number[]) => {
-    const sortedIndexArray = targetIndexArray.sort((a, b) => b - a) // use sort for targetIndexArray inner element from big to small, make sure splicing delete big to small
+    // use sort for targetIndexArray inner element from big to small, make sure splicing delete big to small
+    const sortedIndexArray = targetIndexArray.sort((a, b) => b - a)
     sortedIndexArray.forEach(_sortIndex => {
         source.splice(_sortIndex, 1)
     })
