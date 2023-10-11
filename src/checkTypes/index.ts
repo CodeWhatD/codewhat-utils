bfaebd357062594e430e40d2f85306f9bad37516
myduh <myduh@isoftstone.com>
2023-10-11 16:42:45 +0800

    feat: add utils func isObject

--- src/checkTypes/index.ts
+++ src/checkTypes/index.ts
@@ -1,3 +1,7 @@
 export const isUndefined = (data: unknown) => {
   return typeof data === "undefined";
 };
+
+export const isObject = (data: unknown) => {
+  return data !== null && typeof data === 'object'
+}
