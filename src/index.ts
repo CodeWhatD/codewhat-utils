deb909b381863c5b7428a4e269dce8aa2e43dc6d
myduh <myduh@isoftstone.com>
2024-04-25 09:44:10 +0800

    chore: export convertColumn function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import { isUndefined, isObject, isArray } from "./checkTypes";
 import flatMenu from "./flatMenu";
-import Onion from './onion'
-import { segment } from './segment'
-
-export { isUndefined, isObject, isArray, Onion, flatMenu, segment };
+import Onion from "./onion";
+import { segment } from "./segment";
+import { convertColumn } from "./convertColumn";
+export {
+  isUndefined,
+  isObject,
+  isArray,
+  Onion,
+  flatMenu,
+  segment,
+  convertColumn,
+};
